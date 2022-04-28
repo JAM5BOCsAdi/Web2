d7a1fd236a5fa764b9ab2debf52cd1b034a207ea
Adi <oradam86@gmail.com>
2022-04-28 21:50:07 +0200

    Default config

--- config.php
+++ config.php
@@ -1,25 +1,34 @@
 <?php
 
-// http://localhost/works/web2/index.php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+/*
+
+LOCAL:
+http://localhost/works/web2/index.php
+Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) 
+
 $server = 'localhost';
 $username = 'root';
 $password = '';
 $dbname = 'web2';
  
-/* Attempt to connect to MySQL database */
+ Attempt to connect to MySQL database 
 $link = mysqli_connect($server, $username, $password, $dbname);
  
-// Check connection
+ Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-/* ------------------------- */
-/*
-For ELTE -> mysql.caesar.elte.hu
 
+*/
+
+/*
+Web.elte.hu :
+For ELTE -> mysql.caesar.elte.hu
+ https://oradam.web.elte.hu/web2/leaderboard.php
+ https://info.caesar.elte.hu/iframe-wrapper.php?q=frontendForMys%2F%3Fusername%3Doradam
+*/
 $server = 'mysql.caesar.elte.hu';
 $username = 'oradam';
 $password = 'Pcn30DFTv5zMLcR5';
@@ -32,7 +41,7 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-*/
+
 
 ?>
 
