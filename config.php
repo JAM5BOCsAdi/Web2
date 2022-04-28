9596713dec93b90f2a96d76fb82bf7bc271b24d8
Adi <oradam86@gmail.com>
2022-04-28 21:07:07 +0200

    Default config

--- config.php
+++ config.php
@@ -3,16 +3,36 @@
 // http://localhost/works/web2/index.php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'web2');
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'web2';
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect($server, $username, $password, $dbname);
  
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+/* ------------------------- */
+/*
+
+
+$server = 'mysql.caesar.elte.hu';
+$username = 'oradam';
+$password = 'Pcn30DFTv5zMLcR5';
+$dbname = 'oradam';
+ 
+
+$link = mysqli_connect($server, $username, $password, $dbname);
+
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+*/
+
 ?>
+
